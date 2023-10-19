f1d73b51dd5e9ed01e90a8d431000d4fa65f0c1a
Ubuntu <abhinand@MedMCQA-1.lrtyhaiomcue5kow04h50zuy5b.bx.internal.cloudapp.net>
2023-10-19 04:27:29 +0000

    feat(pretraining): Tamil LLAMA pretraining

--- scripts/training/run_pt.sh
+++ scripts/training/run_pt.sh
@@ -7,14 +7,14 @@ lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
 modules_to_save="embed_tokens,lm_head"
 lora_dropout=0.05
 
-pretrained_model=path/to/hf/llama-2/dir
-chinese_tokenizer_path=path/to/chinese-llama-2/tokenizer/dir
-dataset_dir=path/to/pt/data/dir
-data_cache=temp_data_cache_dir
-per_device_train_batch_size=1
-gradient_accumulation_steps=8
+pretrained_model=/home/abhinand/projects/Chinese-LLaMA-Alpaca-2/models/llms/llama2-7b-hf
+chinese_tokenizer_path=/home/abhinand/projects/Chinese-LLaMA-Alpaca-2/scripts/merge_tokenizer/merged_tokenizer_hf
+dataset_dir=/home/abhinand/projects/Chinese-LLaMA-Alpaca-2/data/culturax_tamil_text
+data_cache=/home/abhinand/projects/Chinese-LLaMA-Alpaca-2/data/cache_dir
+per_device_train_batch_size=64
+gradient_accumulation_steps=1
 block_size=512
-output_dir=output_dir
+output_dir=/home/abhinand/projects/Chinese-LLaMA-Alpaca-2/models/llms/llama2-7b-tamil
 
 deepspeed_config_file=ds_zero2_no_offload.json
 
@@ -24,11 +24,11 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_pt_with_peft.py \
     --tokenizer_name_or_path ${chinese_tokenizer_path} \
     --dataset_dir ${dataset_dir} \
     --data_cache_dir ${data_cache} \
-    --validation_split_percentage 0.001 \
+    --validation_split_percentage 0.75 \
     --per_device_train_batch_size ${per_device_train_batch_size} \
     --do_train \
     --seed $RANDOM \
-    --fp16 \
+    --bf16 \
     --num_train_epochs 1 \
     --lr_scheduler_type cosine \
     --learning_rate ${lr} \
@@ -37,8 +37,8 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_pt_with_peft.py \
     --logging_strategy steps \
     --logging_steps 10 \
     --save_strategy steps \
-    --save_total_limit 3 \
-    --save_steps 200 \
+    --save_total_limit 2 \
+    --save_steps 100 \
     --gradient_accumulation_steps ${gradient_accumulation_steps} \
     --preprocessing_num_workers 8 \
     --block_size ${block_size} \
@@ -51,7 +51,8 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_pt_with_peft.py \
     --trainable ${lora_trainable} \
     --lora_dropout ${lora_dropout} \
     --modules_to_save ${modules_to_save} \
-    --torch_dtype float16 \
+    --torch_dtype bfloat16 \
     --load_in_kbits 16 \
     --gradient_checkpointing \
     --ddp_find_unused_parameters False
+# --resume_from_checkpoint /home/abhinand/projects/Chinese-LLaMA-Alpaca-2/models/llms/llama2-7b-tamil/checkpoint-400
